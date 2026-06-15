cec7671850f4e5f50f07105db7079e18266932c8
vitoriaalex28 <vitoriaalexandre.202117@epff-intep.pt>
2026-06-15 11:55:33 +0100

    Add files via upload

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.Design;
+public class Exercicio9
+{
+    public static void Main(string[] args)
+    {
+        for (int counter = 1; counter <= 20; counter++)
+        {
+            if (counter %2 == 0)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
