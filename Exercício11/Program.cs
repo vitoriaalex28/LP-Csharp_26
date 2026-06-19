2c702425f1e219da14670a14acd0124f76c90870
vitoriaalex28 <vitoriaalexandre.202117@epff-intep.pt>
2026-06-19 09:36:03 +0100

    Add files via upload

---
+++ Exercício11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Exercício11
+{
+    public static void Main(string[] args)
+    {
+        int i = 3;
+        int multi = 1;
+
+        while (i <= 30)
+        {
+            Console.WriteLine("3 x " + multi + "= " + i);
+            i += 3;
+            multi++;
+        }
+        
+      
+    }
+}
