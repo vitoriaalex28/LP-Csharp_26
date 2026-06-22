68b9aa4cff364625bc466f291af08da172beeb79
vitoriaalex28 <vitoriaalexandre.202117@epff-intep.pt>
2026-06-22 13:33:00 +0100

    Add files via upload

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercicio12
+{
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+        int counter;
+
+        for (counter = 0; soma <= 100; counter++)
+        {
+            soma += counter;
+            Console.WriteLine(soma);
+        }
+        Console.WriteLine("Último número adicionado: " + counter);
+    }
+}
