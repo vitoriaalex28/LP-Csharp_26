6dd891ff1ffc862ccbd3cfbecd42219415c1fa85
vitoriaalex28 <vitoriaalexandre.202117@epff-intep.pt>
2026-06-22 13:33:32 +0100

    Add files via upload

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Exercicio13
+{
+     public static void Main(string[] args)
+     {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 5);
+        Console.WriteLine("Contagem Concluída!");
+     }
+            
+    
+}
